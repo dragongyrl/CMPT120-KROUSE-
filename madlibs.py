3322711df2bbf5406749e72d47be8a3da185e0df
dragongyrl <35709398+dragongyrl@users.noreply.github.com>
2018-01-29 17:40:14 -0500

    Solution to Mad Libs excercise.

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+#Instructor: Juan Arias
+#Author: Amelia Krouse
+#Date: 1/29
+noun = input("Please enter a noun.")
+verb = input("Please enter a verb.")
+adj = input("Please enter an adjective.")
+place = input("Please enter a place.")
+print("We", verb, "the", adj, noun, "in", place, ".")
