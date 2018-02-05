f0341854a5c1e178ea515b549796394104f4d3f1
dragongyrl <35709398+dragongyrl@users.noreply.github.com>
2018-02-05 17:46:51 -0500

    Wrote an interactive Fibonacci calculator.

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+#Author: Amelia Krouse
+#Instructor: Juan Arias
+#Date: 2/5
+
+def main():
+    num = int(input("Please enter an integer."))
+    x = 1
+    prev = 0
+    twoprev = 0
+    print(1)
+    for i in range(num -1):
+        result = x + prev
+        twoprev = prev
+        prev = x
+        x = result
+        print(result)
+
+main()
