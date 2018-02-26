945c199263790cb2489cb3bcc3af19afe167405f
dragongyrl <35709398+dragongyrl@users.noreply.github.com>
2018-02-26 17:09:39 -0500

    string things started

---
+++ stringthings.py
@@ -0,0 +1,21 @@
+#Author: Amelia Krouse
+#Instructor: Juan Arias
+
+def main():
+
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+uname = first + '.' + last
+passwd = input("Create a new password: ")
+
+while False:
+    if passwd.len()<8:
+        print("Fool of a Took! That password is feeble!")
+        passwd = input("Create a new password: ")
+    elif:
+        True
+print("The force is strong in this one…")
+print("Account configured. Your new email address is",
+uname + "@marist.edu")
+
+main()
