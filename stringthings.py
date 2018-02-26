86d959eecd812023ebd8a43e7814f3ecbb5ee585
dragongyrl <35709398+dragongyrl@users.noreply.github.com>
2018-02-26 17:14:43 -0500

    changed string things

--- stringthings.py
+++ stringthings.py
@@ -2,20 +2,20 @@
 #Instructor: Juan Arias
 
 def main():
-
-first = input("Enter your first name: ")
-last = input("Enter your last name: ")
-uname = first + '.' + last
-passwd = input("Create a new password: ")
-
-while False:
-    if passwd.len()<8:
-        print("Fool of a Took! That password is feeble!")
-        passwd = input("Create a new password: ")
-    elif:
-        True
-print("The force is strong in this one…")
-print("Account configured. Your new email address is",
-uname + "@marist.edu")
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+    uname = first + '.' + last
+    passwd = input("Create a new password: ")
+    boo=False
+    
+    while boo == False:
+        if len(passwd) < 8:
+            print("Fool of a Took! That password is feeble!")
+            passwd = input("Create a new password: ")
+        else:
+              boo=True
+    print("The force is strong in this one…")
+    print("Account configured. Your new email address is",
+    uname + "@marist.edu")
 
 main()
